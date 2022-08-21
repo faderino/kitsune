e3cfe4627d75e3fb64565f78ab29367f60f16da7
fadi <fadi99.email@gmail.com>
2022-08-22 00:30:05 +0700

    Add transparent color for header

--- styles/colors.ts
+++ styles/colors.ts
@@ -4,3 +4,4 @@ export const magenta = '#ff2882';
 export const text = '#242424';
 export const textLighten = '#6c6c6c';
 export const textLight = '#bcbedc';
+export const seeThrough = 'rgba(35, 38, 67, 0.5)';
